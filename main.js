9a1491ada649e81c86b615e1dd81073ed236128a
PrintRonald <madariaga492@gmail.com>
2021-08-17 23:56:35 -0400

    Trabajo individual jquery
    Se agrega carrusel a la página de rutas.

--- main.js
+++ main.js
@@ -20,3 +20,17 @@ function changeView(type) {
 
   }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const elementosCarousel = document.querySelectorAll('.carousel');
+  M.Carousel.init(elementosCarousel, {
+      duration: 1500,
+      dist:-80,
+      shift: 5,
+      padding: 5,
+      numVisible:3,
+      indicators: true,
+      nowRap: false,
+
+  });
+});
